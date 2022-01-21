52b45b2ac92be87d7a405f51416b68ee99e1cef3
nakajima-satoru <nakajima@email.com>
2022-01-22 00:20:08 +0900

    1/21

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+/**
+ * ===================================================================================================
+ * Hachiware_tool
+ * 
+ * Node.js new web framework.
+ * 
+ * License : MIT License. 
+ * Since   : 2022.01.21
+ * Author  : Nakatsuji Masato 
+ * Email   : nakatsuji@teastalk.jp
+ * HP URL  : https://hachiware-js.com/
+ * GitHub  : https://github.com/masatonakatsuji2021/Hachiware
+ * npm     : ?????
+ * ===================================================================================================
+ */
 
 const sync = require("hachiware_sync");
 const tool = require("hachiware_tool");
---
+++ install.js
@@ -0,0 +1,18 @@
+/**
+ * ===================================================================================================
+ * Hachiware_tool
+ * 
+ * Node.js new web framework.
+ * 
+ * License : MIT License. 
+ * Since   : 2022.01.21
+ * Author  : Nakatsuji Masato 
+ * Email   : nakatsuji@teastalk.jp
+ * HP URL  : https://hachiware-js.com/
+ * GitHub  : https://github.com/masatonakatsuji2021/Hachiware
+ * npm     : ?????
+ * ===================================================================================================
+ */
+
+var rootPath = __dirname;
+exec("set %path% = %path%" + rootPath);
